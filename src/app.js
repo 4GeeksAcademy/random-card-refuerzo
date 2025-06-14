97bb8f29af6ded7f992f958b0a161c9b496faf1b
Alcides Contreras <alcidescon@gmail.com>
2025-06-14 16:11:18 +0000

    cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let symbolsTags = document.querySelectorAll('.card-symbol')
+  let textCard = document.getElementById('card-text')
+
+  const numbers = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K' ]
+  const symbols = ['♥', '♠', '♦', '♣']
+
+  const getRandom = (array) => {
+    return array[Math.floor(Math.random() * array.length)]
+  }
+
+  const randomSymbol = getRandom(symbols);
+
+  textCard.textContent = getRandom(numbers)
+  symbolsTags.forEach(symbol => {
+    symbol.textContent = randomSymbol;
+    symbol.textContent === '♥' || symbol.textContent === '♦' ? symbol.style.color = 'red' : ''
+  });
 };
